685206ca18342901820c095aeb68656c8a2314ef
Andri <a8461565@gmail.com>
2024-02-07 01:19:20 +0700

    first commit

--- app/api/frontend/route.ts
+++ app/api/frontend/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    const response = await fetch("http://localhost:1337/api/frontends");
+    const response = await fetch(`https://appealing-hope-d4588306a3.strapiapp.com/api/frontends?timestamp=${Date.now()}`);
 
     if (!response.ok) {
       throw new Error("Failed to fetch data");
@@ -10,7 +10,7 @@ export const GET = async (req: NextRequest) => {
 
     const data = await response.json();
 
-    return NextResponse.json(data, { revalidate: 1 } as any);
+    return NextResponse.json(data, { revalidate: 10 } as any);
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json({ error: "Failed to fetch data" }, {
